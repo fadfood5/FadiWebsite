7c9f92d56f7f7390a3ccc2571ba44cc2138ea6ac
Fadi Bitar <fadibitar@mail.usf.edu>
2022-02-16 23:11:28 -0500

    fixed typeit error

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+                            $('.typeName').typeIt({
+                                strings: ["Fadi Bitar"],
+                                speed: 120,
+                                breakLines: false,
+                                autoStart: true,
+                                loop: false,
+                            });
